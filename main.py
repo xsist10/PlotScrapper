229f360356de8fab902cbf21c0da6cd6518f9d8a
xsist10 <xsist10@gmail.com>
2023-08-05 22:30:36 +0200

    Updated max price to 5000 GBP per acre

--- main.py
+++ main.py
@@ -16,7 +16,7 @@ def examine_houses(houses):
             continue
 
         # We skip POA (since they're a pain to get) and anything that costs more than 10,000 GBP per acre
-        if house.guide_price() == 'POA' or house.price_per_acre() > 6000:
+        if house.guide_price() == 'POA' or house.price_per_acre() > 5000:
             print(f" - Skipped due to price per acre: {house.price_per_acre()}")
             continue
 
